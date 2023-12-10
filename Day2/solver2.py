1fae5fbac93807ad13984225aacdd638d0c2a8b4
Subash1237 <rimalsubash77@gmail.com>
2023-12-10 17:40:16 +0545

    day 2 part 2 is also completed.

---
+++ Day2/solver2.py
@@ -0,0 +1,26 @@
+
+import re
+# So, 12 red , 13 green and 14 blue have total of 39 
+
+r=12
+g=13
+b=14
+
+def main():
+    sum=0
+    with open("day_2.txt") as f:
+        data=f.readlines()
+        for i in data:
+            a=re.match(r'Game (\d+)',i)
+            if a:
+                print(re.findall(r'(\d+) green',i))
+                green=max(map(int,re.findall(r'(\d+) green',i)))
+                red=max(map(int,re.findall(r'(\d+) red',i)))
+                blue=max(map(int,re.findall(r'\b(\d+) blue',i)))
+                sum+=green*blue*red 
+                # if (green <= g and red <= r and blue <= b):
+                    # sum+=int(a.group(1))
+    print(sum) 
+
+if __name__=='__main__' :
+    main()
