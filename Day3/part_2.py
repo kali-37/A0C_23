b4da372d9844b70b2447a2056ada45b4b771c70b
Subash1237 <rimalsubash77@gmail.com>
2023-12-13 14:55:17 +0545

    day 3 completed.

--- Day3/part_2.py
+++ Day3/part_2.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
+
 def main():
     with open("input.txt","r") as f:
         dataset=f.readlines() 
-    sume=0
     jk=len(dataset)
-    satter=[]
+    gears:dict[tuple[int,int],list[int]]=defaultdict(list)
     for j in range(jk):
         n=0
         detected=False
+        detecter=(0,0)
         for i in range(len(dataset[j])):
             if dataset[j][i].isdigit():
                 n=n*10+int(dataset[j][i])
@@ -14,30 +16,22 @@ def main():
                     for col in [-1,0,1]:
                         if 0<=j+row<=jk-1 and 0<=i+col<=len(dataset[0])-1:
                             wilb=dataset[j+row][i+col]
-                            if  not wilb.isdigit() and not wilb=='.' and wilb!="\n":
+                            if  not wilb.isdigit() and not wilb=='.\n' and wilb=="*":
                                 detected=True
+                                detecter=(j+row,col+i)
             elif   detected:
-                satter.append(n) 
-                sume+=n 
+                gears[detecter].append(n)
                 detected=False
                 n=0
 
             else :
                 n=0
-    print(sume,satter)
-                    
-            
+    sum=0
+    for i in gears.values():
+        if len(i)==2: 
+            sum+=i[0]*i[1]
+    print(sum)
 
 
-
-
-
-
-
-        
 if __name__=="__main__":
     main()
-
-
-
-
