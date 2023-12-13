e972c4067bce10c8bc58492ad53d4f50b5ba20fd
Subash1237 <rimalsubash77@gmail.com>
2023-12-13 19:25:00 +0545

    part 1 optminze...d

--- Day4/part1.py
+++ Day4/part1.py
@@ -1,9 +1,8 @@
 from sys import argv
 import re
-
+# trying to use split method rather ,then regex and regex too..
 def main(dataset):
     frontline: dict[tuple,set]={}
-    # backline: dict[set,set]={}
     TEMP1=""
     TEMP2=""
     pattern=r'\|\s*([\d\s]+)'
@@ -15,21 +14,11 @@ def main(dataset):
             TEMP2=TEMP2.group(1).split()
             TEMP2=set(map(int,TEMP2)) 
             frontline[TEMP1]=TEMP2
-
     length=0
-    print(frontline)
     for i in frontline:
-        # print(set(i),frontline[i])
-        # print(frontline[i],"   ... ",set(i))
-        
         lne=(len(frontline[i].intersection(set(i))))
-        print(lne)
-        # if lne>1:
         if lne:
             length+=2**(lne-1)
-        # else:
-            # length+=lne
-
     print(length)
 
 if __name__=="__main__":
