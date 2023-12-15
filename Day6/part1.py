f876e4d4db038aa32f721db3d282bb3fff61f498
Subash1237 <rimalsubash77@gmail.com>
2023-12-15 22:10:27 +0545

    Day 6 part 1 completed..

---
+++ Day6/part1.py
@@ -0,0 +1,57 @@
+from sys import argv 
+
+# creating own class of _zip functionality to see how iter() works under the hood .
+# we can directly use zip() in pyhton to zip the stuffs , by the way zip class is additional.
+class _zip():
+    def __init__(self,*args):
+        self.iters=args 
+        self.iter_count=len(args) 
+        self.curr_index=0
+    
+    def __iter__(self):  
+        return self.calculate_zip()
+    
+    def calculate_zip(self):
+        _iters=[iter(its) for its in self.iters] 
+        while(True):
+            try:
+                new_nexts=[next(it) for it in _iters ]
+                yield tuple(new_nexts)
+            except StopIteration:
+                return
+
+def __t_d_map(__given_time,__given_dist): # 
+
+    mapper:dict[int,int]={}    
+    for i in _zip(__given_time,__given_dist):
+        max_dist=i[1]
+        max_time=i[0]
+        print(i)
+        limit_gone=[]
+        for j in range(max_time+1):
+            limit_gone.append(j*(max_time-j))
+        mapper[i]=len(list(filter(lambda x:x>max_dist,limit_gone)))
+    
+    mul=1
+    for i in mapper.values():
+        mul*=i 
+    print(mul)
+        
+
+
+def main(dataset):
+    __given_time=list(map(int,dataset[0].split()[1:]))
+    __given_dist=list(map(int,dataset[1].split()[1:]))
+    __given_dist.append(123)
+    __t_d_map(__given_time,__given_dist)
+    ...
+
+
+if __name__=="__main__":
+    if len(argv)>=2:
+        with open(argv[1],"r") as f :
+            
+            dataset=f.readlines()
+            main(dataset)
+    else:
+        print("Usage: python3 part__.py <input.txt>")  
