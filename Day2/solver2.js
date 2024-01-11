271d66db140838b2ca56fa9e87c0a97b0c6acedc
Subash1237 <rimalsubash77@gmail.com>
2024-01-11 22:47:52 +0545

    day 2 completed part 2 too.

---
+++ Day2/solver2.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+let data_file=""
+const r=12;
+const b=14;
+const g=13;
+async function readfile(){
+    try{
+        data_file=(await fs.promises.readFile('day_2.txt',{encoding:"utf-8"})).split('\n');
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+async function main(){
+    let sum=0;
+    let game_number="";
+    await readfile();        
+    for(let i of data_file)
+    {
+        dicter={}
+        const regex0=new RegExp("Game\\s(\\d+):*");
+        game_number=i.match(regex0)[1];
+        const regex=new RegExp("\\d+ \\w+","g")
+        let matchr=[...i.match(regex)]
+            for (let j of  matchr){
+            let [Num ,value]=j.split(' ')
+            // console.log("NUM: ",Num);
+            if (!dicter[value]){
+            dicter[value]=-1
+            }
+            if (dicter[value]<+Num){
+                dicter[value]=+Num;
+            }
+                }
+            sum+=(+dicter['red']*+dicter['green'] *+dicter['blue']);
+            }   
+ 
+    console.log(sum)
+    }
+    // console.log(data_file);
+main()
